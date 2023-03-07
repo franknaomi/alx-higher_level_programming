dd185758d213a1ee888fc979c7aeb0993361d552
Frank Naomi <franknaomi0@gmail.com>
2023-03-07 23:07:54 +0000

    No 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-for n in range(1, 101):
-if n % 15 == 0:
-print("FizzBuzz", end=" ")
-continue
-if n % 3 == 0:
-print("Fizz", end=" ")
-if n % 5 == 0:
-print("Buzz", end=" ")
-if n % 3 != 0 and n % 5 != 0:
-print(n, end=" ")
+	for n in range(1, 101):
+	  if n % 15 == 0:
+		print("FizzBuzz", end=" ")
+		continue
+	  if n % 3 == 0:
+		print("Fizz", end=" ")
+	  if n % 5 == 0:
+		print("Buzz", end=" ")
+	  if n % 3 != 0 and n % 5 != 0:
+		print(n, end=" ")
