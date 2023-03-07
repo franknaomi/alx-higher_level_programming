d12af8b929535c42c41cbdf3f4d6719419766289
Frank Naomi <franknaomi0@gmail.com>
2023-03-07 23:54:30 +0000

    Tasks 3 and 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in range(97, 123):
-if chr(n) != 'q' and chr (n) != 'e':
-print("{}".format(chr(n)), end="")
+    if chr(n) != 'q' and chr (n) != 'e':
+        print("{}".format(chr(n)), end="")
