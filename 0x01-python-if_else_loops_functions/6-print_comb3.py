fa9def550f18918f2b68e002c0acb486192db861
Frank Naomi <franknaomi0@gmail.com>
2023-03-07 23:41:23 +0000

    Tasks 5 and 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for a in range(10):
-    for b in range(10):
-        if b > a and a != 8:
-            print("{}{}".format(a, b), end=", ")
-print(89)
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
