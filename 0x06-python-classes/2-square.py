3e3e3875cb762dd7a767296b9dc10842bd577fa5
Frank Naomi <franknaomi0@gmail.com>
2023-03-28 18:58:17 +0000

    Edited Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
-
 class Square:
     """Represent a square."""
 
@@ -14,4 +13,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size = size
